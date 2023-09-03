b7d349371720d43363f99612290b64f24fffef21
singh-jagjot <jagjot.no1@gmail.com>
2023-09-03 19:35:24 -0400

    added closest centroid

--- KMeans.py
+++ KMeans.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class KMeans:
-    def __init__(self, K: int, X) -> None:
+    def __init__(self, K: int, X:np.ndarray) -> None:
         self.K = K
         self.X = X
         self.centroids = self._init_centroids()
@@ -16,6 +16,18 @@ class KMeans:
         
         # Selecting K elements from X using rand_idx
         return self.X[rand_idx[:self.K]]
+    
+    def _find_closest_centroid(self):
+        centroid_idx = np.zeros(self.X.shape[0], dtype=int)
+        for i in range(self.X.shape[0]):
+            distance = float('inf')
+            for j in range(self.centroids.shape):
+                l2norm = np.linalg.norm(self.X[i] - self.centroid[j])
+                if l2norm < distance:
+                    distance = l2norm
+                    centroid_idx[i] = j
+
+        return centroid_idx
     
     
 
