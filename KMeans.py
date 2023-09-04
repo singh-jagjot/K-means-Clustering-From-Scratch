a83e0b47694be06cc81adea2fb2837252fb50172
singh-jagjot <jagjot.no1@gmail.com>
2023-09-04 06:39:14 -0400

    Added faster and stable code

--- KMeans.py
+++ KMeans.py
@@ -1,12 +1,12 @@
 import numpy as np
-
+import time
 
 class KMeans:
     def __init__(self, X: np.ndarray, K: int, max_iters: int) -> None:
-        self.K = K
         self.X = X
+        self.K = K
         self.max_iters = max_iters
-        self.centroids = self._init_centroids()
+        # self.centroids = self._init_centroids()
 
     def _init_centroids(self):
         m, _ = self.X.shape
@@ -18,35 +18,77 @@ class KMeans:
         # Selecting K elements from X using rand_idx
         return self.X[rand_idx[:self.K]]
 
-    def _find_closest_centroid(self):
-        centroid_idx = np.zeros(self.X.shape[0], dtype=int)
-        for i in range(self.X.shape[0]):
-            min_distance = float('inf')
-            for j in range(self.centroids.shape[0]):
-                l2norm = np.linalg.norm(self.X[i] - self.centroid[j])
-                if l2norm < min_distance:
-                    min_distance = l2norm
-                    centroid_idx[i] = j
+    def _find_closest_centroid(self, centroids):
+        K = centroids.shape[0]
+        # centroid_idx = np.zeros(self.X.shape[0], dtype=int)
+        # centroid_idx2 = np.zeros(self.X.shape[0], dtype=int)
+        centroid_idx3 = np.zeros(self.X.shape[0], dtype=int)
+        # t = time.time()
+        # for i in range(self.X.shape[0]):
+        #     min_distance = float('inf')
+        #     for j in range(K):
+        #         l2norm = np.linalg.norm(self.X[i] - centroids[j])
+        #         if l2norm < min_distance:
+        #             min_distance = l2norm
+        #             centroid_idx[i] = j
+        # print("logic 1: ", time.time()-t)
 
-        return centroid_idx
+        # t = time.time()
+        # for i in range(self.X.shape[0]):
+        #     centroid_idx2[i] = np.argmin(np.linalg.norm(centroids - self.X[i], axis=1))
+        # print("logic 2: ", time.time()-t)
 
-    def _compute_centroids(self):
-        centroid_idx = self._find_closest_centroid()
-        centroid_sum = np.zeros_like(self.centroids)
-        centroid_total_ele = np.zeros(self.centroids.shape[0])
+        # t = time.time()
+        ci = np.zeros((K, self.X.shape[0]))
+        for i in range(K):
+            ci[i] = np.linalg.norm(self.X - centroids[i], axis=1)
+        centroid_idx3 = np.argmin(ci, axis=0)
+        # print("logic 3: ", time.time()-t)
+
+        # print("1 == 2: ", np.array_equal(centroid_idx, centroid_idx2))
+        # print("1 == 3: ", np.array_equal(centroid_idx, centroid_idx3))
+        # print("2 == 3: ", np.array_equal(centroid_idx2, centroid_idx3))
+        print("     Orphan Centroids: ", K - np.unique(centroid_idx3).shape[0])
+        return centroid_idx3
+
+    def _compute_centroids(self, centroid_idx:np.ndarray, centroids:np.ndarray):
+        centroid_sum = np.zeros_like(centroids)
+        centroid_total_ele = np.zeros(centroids.shape[0])
         for i in range(self.X.shape[0]):
             centroid_sum[centroid_idx[i]] += self.X[i]
-            centroid_total_ele[i] += 1
+            centroid_total_ele[centroid_idx[i]] += 1
+        # print(centroid_total_ele)
+        # print(centroid_sum)
+        centroids = centroid_sum / centroid_total_ele.reshape((centroid_total_ele.shape[0], 1))
+        return np.nan_to_num(centroids)
 
-        return centroid_sum / centroid_total_ele.reshape((centroid_total_ele, 1))
-
-    def _compute_cost(self):
-        centroid_idx = self._find_closest_centroid()
+    def _compute_cost(self, centroids:np.ndarray,centroid_idx:np.ndarray):
         cost = 0
         for i in range(self.X.shape[0]):
-            distance = np.linalg.norm(self.X - self.centroids[centroid_idx[i]])
+            distance = np.linalg.norm(self.X[i] - centroids[centroid_idx[i]])
             cost += distance
-        return cost
+        return cost / centroids.shape[0]
 
-    def start(self):
-        
+    def start(self, kmeans_iter:int = 1):
+        kmeans_iter_cost = np.zeros(kmeans_iter)
+        kmeans_iter_values = []
+        for iter in range(kmeans_iter):
+            centroid_idx = None
+            centroids = self._init_centroids()
+            previous_cost = float("inf")
+            print("K-Means Iteration: ", iter+1)
+            for i in range(self.max_iters):
+                print(" Iteration: ", i+1)
+                centroid_idx = self._find_closest_centroid(centroids)
+                centroids = self._compute_centroids(centroid_idx, centroids)
+                cost = self._compute_cost(centroids, centroid_idx)
+                print("     Cost: ", cost)
+                if cost >= previous_cost:
+                    break
+                previous_cost = cost
+            kmeans_iter_cost[iter] = cost
+            kmeans_iter_values.append((centroids, centroid_idx))
+        print("Costs: ", kmeans_iter_cost)
+        print("Arg min: ", np.argmin(kmeans_iter_cost))
+        return kmeans_iter_values[np.argmin(kmeans_iter_cost)]
+    
